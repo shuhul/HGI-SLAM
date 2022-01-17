f0f0cd8915950e78551dd51e4b7d07b502c638b7
shuhul <shuhulmujoo@gmail.com>
2022-01-17 13:17:50 -0800

    Created python concurrentTest, untested, also try to only build some not all files

--- orbslam/orbslam.sh
+++ orbslam/orbslam.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 cd /root/ORB_SLAM2/
-./build.sh
+cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release
+make -j
+# ./build.sh
+cd ..
 cd Examples/Monocular/
 ./mono_tum ../../Vocabulary/ORBvoc.txt TUM1.yaml /root/ORB_FR1
+
+# cd /root/ORB_SLAM2/build/CMakeFiles/
+# make -j mono_tum.dir/build.make
+# cd /root/ORB_SLAM2/Examples/Monocular/
+# ./mono_tum ../../Vocabulary/ORBvoc.txt TUM1.yaml /root/ORB_FR1
+
