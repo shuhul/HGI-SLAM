f840b70fccbfab5aca4b4ecf3c22c6d53808cb5f
shuhul <shuhulmujoo@gmail.com>
2022-02-25 15:21:30 -0800

    New Wsave

--- wsave.sh
+++ wsave.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd /root/HGI_SLAM/
 git add .
-git reset superpoint/saved/descriptor_list.obj
-git reset salgan/saved/descriptor_list.obj
+git reset "*.obj"
 git commit -m "$1"
 git push
