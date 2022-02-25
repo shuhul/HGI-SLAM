686b57167ece3dd4cf14a5edfcd6dc46cfa52f93
shuhul <shuhulmujoo@gmail.com>
2022-02-24 21:06:42 -0800

    Kind of works

--- common/handler.py
+++ common/handler.py
@@ -104,27 +104,51 @@ def showVideo():
             break
     cv2.destroyAllWindows()
 
-def showTrajectory(start=0, stop=-1, showGT = True, showB = False):
-    xs, zs = readKFT('before')
+def showTrajectory(showGT = True, showB = False, showLC = True):
+    bxs, bzs = readKFT('before')
     axs, azs = readKFT('after')
     txs, tzs = readGT()
-    xs = xs[start:stop]
-    zs = zs[start:stop]
-    axs = axs[start:stop]
-    azs = azs[start:stop]
+
+    lcInd = len(bxs)-3
+    if showLC:
+        axs = axs[lcInd:-1]
+        azs = azs[lcInd:-1]
+        lcxs = [bxs[-1],axs[0]]
+        lczs = [bzs[-1],azs[0]]
+        shaxs = list(np.array(axs) + (lcxs[0]-axs[0]))
+        shazs = list(np.array(azs) + (lczs[0]-azs[0]))
+
+    black = col(33,41,48, 0.7)
+    lightblue = col(0, 53, 178, 0.8)
+    red = col(220, 32, 52, 0.9)
+    lightred = col(220, 32, 52, 0.5)
+    green = col(11, 68, 31, 0.95)
+
     if showGT:
-        plt.plot(txs, tzs, color='black', linewidth=1.0)
+        plt.plot(txs, tzs, color=black, linewidth=1.0)
     if showB:
-        plt.plot(xs, zs, color='blue', linewidth=1.0)
-    plt.plot(axs, azs, color='red', linewidth=1.0)
+        plt.plot(bxs, bzs, color=lightblue, linewidth=2.0)
+
+    plt.plot(axs, azs, color=red, linewidth=2.0)
+
+    if showLC:
+        plt.plot(shaxs, shazs, color=lightred , linewidth=2.0)
+        plt.plot(lcxs, lczs, color=green, linewidth=3.0)
+        
+    
     plt.xlabel('x [m]')
     plt.ylabel('y [m]')
     plt.savefig('kft.png')
-    cv2.imshow('Trajectory', cv2.imread('kft.png', cv2.IMREAD_COLOR))
+    name = 'HGI-SLAM Trajectory'
+    cv2.namedWindow(name, cv2.WINDOW_NORMAL)
+    cv2.resizeWindow(name, 640, 560)
+    cv2.imshow(name, cv2.imread('kft.png', cv2.IMREAD_COLOR))
     while True:
         if(cv2.waitKey(0) & 0xFF == ord('q')):
             break
 
+def col(r, g, b, a):
+    return list(np.array([r, g, b, a*255])/255)
 def saveCurrentIndex(index):
     global currentIndex
     currentIndex = index
@@ -197,8 +221,8 @@ def readKFT(filename):
     origin = (1.4, 1.7)
     theta = np.deg2rad(-40)
     scale = 0.6
-    shutil.copyfile(f'/root/ORB_SLAM2/Examples/Monocular/{filename}.txt', f'{saved_folder}/trajectory.txt')
-    with open(f'{saved_folder}/trajectory.txt', 'r') as kftFile:
+    shutil.copyfile(f'/root/ORB_SLAM2/Examples/Monocular/{filename}.txt', f'{saved_folder}/{filename}.txt')
+    with open(f'{saved_folder}/{filename}.txt', 'r') as kftFile:
         lines = kftFile.readlines()
         for line in lines:
             data = line.split(" ")
